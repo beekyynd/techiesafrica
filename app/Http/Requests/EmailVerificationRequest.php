30f3a41ce177fe60643415cccd4470e89b1627f5
Francis Kalu <snapchainx@gmail.com>
2025-06-07 19:13:25 +0100

    Added email verification

---
+++ app/Http/Requests/EmailVerificationRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Auth\EmailVerificationRequest as CoreRequest;
+
+class EmailVerificationRequest extends CoreRequest
+{
+   public function user($guard = null): Collection
+   {
+       return User::find($this->route('id'));
+   }
+}
---
+++ resources/views/email/verified.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+
+    <div class="text-emerald-500 h-screen w-screen flex justify-center items-center text-md">
+        Your email address has been verified successfully. You can now login to your account.
+    </div>
+
+</x-layout>

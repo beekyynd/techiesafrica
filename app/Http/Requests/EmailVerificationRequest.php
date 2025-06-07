eaae1ea953f3d55fd4700c51ecb3621a97c9c6e0
Francis Kalu <snapchainx@gmail.com>
2025-06-08 00:19:15 +0100

    fixed email verification error

--- app/Http/Requests/EmailVerificationRequest.php
+++ app/Http/Requests/EmailVerificationRequest.php
@@ -5,11 +5,11 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Foundation\Auth\EmailVerificationRequest as CoreRequest;
+use \Illuminate\Foundation\Auth\EmailVerificationRequest as CoreRequest;
 
 class EmailVerificationRequest extends CoreRequest
 {
-   public function user($guard = null): Collection
+   public function user($guard = null)
    {
        return User::find($this->route('id'));
    }

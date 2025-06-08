c59904ac478ba2ea16811eae25fe7763e949002f
Francis Kalu <snapchainx@gmail.com>
2025-06-08 14:04:52 +0100

    api fix

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ Route::middleware("guest")->group(function () {
     Route::post('/login', [AuthenticatedSessionController::class, 'store'])
     ->middleware('guest')
     ->name('login');
+
+    Route::get('/operations/credits', function () {
+    return response()->json([
+        'operations' => App\Enums\OperationEnum::listOfCredits()
+    ]);
+});
+    
 });
 
 Route::middleware(['auth:sanctum'])->group(function () {
@@ -23,12 +30,6 @@ Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::get('/operations/credits', function () {
-    return response()->json([
-        'operations' => App\Enums\OperationEnum::listOfCredits()
-    ]);
 });
 
 });

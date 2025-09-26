4206e69d4410cf7ce494073d65e390b40e00cea0
Francis Kalu <snapchainx@gmail.com>
2025-09-26 18:26:00 +0100

    Made some changes

--- resources/views/email/verified.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-
-    <div class="text-emerald-500 h-screen w-screen flex justify-center items-center text-md">
-        Thank you for verifying your email. You can now login to your account.
-    </div>
-
-</x-layout>
